35613616bcf54620d1b956103f9d9d33e94019e0
hiroaki <hiroaki51@gmail.com>
2018-10-03 16:57:27 +0900

    競合の修正
    ローカルのマスターからブランチを作ったのだが、そのマスターがリモートのものより古かったようで、それを新しいものに合わせた

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-<<<<<<< HEAD
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-=======
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
->>>>>>> master
   has_many :messages
   has_many :group_users
   has_many :groups, through: :group_users
