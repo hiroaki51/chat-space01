e5a38dce54e95292a3ec2c00eff10790c6daac18
hiroaki <hiroaki51@gmail.com>
2018-10-12 16:57:48 +0900

    イベントが発火したときにAjaxを使用して、messages#createが動くようにする

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,3 +1,14 @@
 $('.form__submit').on('click', function(e) {
     e.preventDefault();
+
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
 });
