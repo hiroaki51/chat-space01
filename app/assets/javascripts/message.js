05dcb3d0c3b07ff6d7af40ac9b92b5c6c53b8d5f
hiroaki <hiroaki51@gmail.com>
2018-10-12 21:46:16 +0900

    返ってきたJSONをdoneメソッドで受取り、HTMLを作成する

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,14 +1,45 @@
-$('.form__submit').on('click', function(e) {
-    e.preventDefault();
+$(function(){
 
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
+    function buildHTML(message){
+        var html = `<div class="message">
+                        <div class="upper-message">
+                            <div class="upper-message__user-name">
+                                <%= message.user.name %>
+                            </div>
+                            <div class="upper-message__date">
+                                <%= message.created_at %>
+                            </div>
+                        </div>
+                        <div class="lower-message">
+                            <% if message.content.present? %>
+                                <p class="lower-message__content">
+                                    <%= message.content %>
+                                </p>
+                            <%= image_tag message.image.url, class=" lower-message__image" if message.image.present? %>
+                        </div>`
+
+        return html;
+    }
+
+    $('.form__submit').on('click', function(e) {
+        e.preventDefault();
+
+        var formData = new FormData(this);
+        var url = $(this).attr('action');
+
+        $.ajax({
+            url: url,
+            type: "POST",
+            data: formData,
+            dataType: 'json',
+            processData: false,
+            contentType: false
+        })
+        .done(function(data){
+            var html = buildHTML(data);
+            $('.message').append(html)
+            $('.form__message').val('')
+        })
+
     })
 });
