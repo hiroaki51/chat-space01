c1f12fb15387db70ee066713fa3f60589fe1e0cd
hiroaki <hiroaki51@gmail.com>
2018-10-16 19:14:07 +0900

    修正：ルーティングなどのAPIの準備

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
 
   def index
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(10)
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def edit
@@ -13,10 +18,6 @@ class UsersController < ApplicationController
       render :edit
     end
   end
-
-  def search
-  end
-
 
   private
 
