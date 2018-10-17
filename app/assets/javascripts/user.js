14b8e3075552b3bbc90ae7df113b431b3137237c
hiroaki <hiroaki51@gmail.com>
2018-10-17 18:51:06 +0900

    HTMLをビュー上に追加

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,12 +1,13 @@
 $(function(){
+    var search_list = $(".js-user-search-result");
 
-    function buildHTML(user){
+    function appendUser(user){
         var html = `<div class="chat-group-user clearfix">
                         <p class="chat-group-user__name">${user.name}</p>
                         <a class="user-search-add chat-group-user__btn chat-group-user__btn--add js-add-btn" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
-                        ::after
                     </div>`
-        return html;
+        console.log('html', html);
+        search_list.append(html);
     }
 
     $('#user-search-field').on('keyup', function(e) {
@@ -19,13 +20,10 @@ $(function(){
             dataType: 'json'
         })
         .done(function(data){
-            $('.js-user-search-result').empty();
-            console.log("data:", data);
+            $(".js-user-search-result").empty();
             if (data !== null) {
-                data.forEach(function(data){
-                    var html = buildHTML(data);
-                    console.log("html:", html);
-                    $('.js-user-search-result').append(html);
+                data.forEach(function(user){
+                    appendUser(user);
                 });
             }
         })
