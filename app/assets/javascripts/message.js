a29a4bf5d7844cf355b0aa0a2108191d88335a10
hiroaki <hiroaki51@gmail.com>
2018-10-18 13:05:51 +0900

    修正：レビューでのご指摘部分

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -33,7 +33,6 @@ $(function(){
         e.preventDefault();
         var formData = new FormData(this);
         var url = $(this).attr('action');
-        console.log(url);
         $.ajax({
             url: url,
             type: "POST",
--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -6,7 +6,6 @@ $(function(){
                         <p class="chat-group-user__name">${user.name}</p>
                         <a class="user-search-add chat-group-user__btn chat-group-user__btn--add js-add-btn" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
                     </div>`
-        console.log('html', html);
         search_list.append(html);
     }
 
