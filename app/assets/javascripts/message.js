5c114cf1763bd171a66c76ec2b5c4215df996a30
hiroaki <hiroaki51@gmail.com>
2018-10-19 20:40:17 +0900

    temp1

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -33,7 +33,6 @@ $(function(){
         e.preventDefault();
         var formData = new FormData(this);
         var url = $(this).attr('action');
-
         $.ajax({
             url: url,
             type: "POST",
