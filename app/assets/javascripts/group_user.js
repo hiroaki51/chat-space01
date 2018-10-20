b350edb922f1649f2ca29db081e96fb418a114f2
hiroaki <hiroaki51@gmail.com>
2018-10-20 14:10:00 +0900

    modified minor errors

--- app/assets/javascripts/group_user.js
+++ app/assets/javascripts/group_user.js
@@ -2,7 +2,7 @@ $(function(){
   var groupuser = $("#chat-group-users");
 
   function appendUser(user_id, user_name){
-    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+    var html = `<div class='chat-group-user clearfix js-chat-member' id="chat-group-user-${user_id}">
                   <input name='group[user_ids][]' type='hidden' value=${user_id}></input>
                   <p class='chat-group-user__name'>${user_name}</p>
                   <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
